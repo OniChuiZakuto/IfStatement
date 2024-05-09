9b0460a0b36e0a55731e5f1d716c2b98c1a7e9dd
Ben <torresjanbenidect@gmail.com>
2024-05-09 17:09:12 +0800

    Simple If Statement

---
+++ src/IfCode.java
@@ -0,0 +1,21 @@
+
+public class IfCode {
+
+	public static void main(String[] args) {
+		
+		int age = 100;
+		
+		if(age==100) {
+			System.out.println("Happy Century ");
+		}
+		else if(age<18) {
+			System.out.println("You are a minor");
+		}
+		else if(age>=18) {
+			System.out.println("You are an adult");
+		} 
+		else {
+			System.out.println("What are you?!");
+		}
+	}
+}
