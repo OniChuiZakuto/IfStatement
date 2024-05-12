a645f0f31af257cc49eabcf8cc468f1c30addaab
Ben <torresjanbenidect@gmail.com>
2024-05-12 15:02:09 +0800

    Updated Statements
    Reconstructed switch and if statements

--- src/IfCode.java
+++ src/IfCode.java
@@ -1,26 +1,12 @@
 import javax.swing.JOptionPane;
 import java.util.Scanner;
-
 public class IfCode {
-
-	public static void main(String[] args) {
-		
-		int age = 100;
-		
-		if(age==100) {
-			System.out.println("Happy Century ");
-		}
-		else if(age<18) {
-			System.out.println("You are a minor");
-		}
-		else if(age>=18) {
-			System.out.println("You are an adult");
-		} 
-		else {
-			System.out.println("What are you?!");
-		}
-		
-		String M = JOptionPane.showInputDialog("Name");
+	public static void main(String[] args) {		
+		String[] options = {"yes", "no", "I don't know"};
+		int T = JOptionPane.showOptionDialog(null, "Are you a person", "Confirmation", 0, 3, null, options, options[0]);
+		switch (T) {
+			case 0: 
+			String M = JOptionPane.showInputDialog("Name");
 		switch (M) {
 			case "Mikoto": int m = 9;
 			if (m==9) {
@@ -31,9 +17,18 @@ public class IfCode {
 			if (j==1) {
 				System.out.println("Jansen");
 				JOptionPane.showMessageDialog(null, "Hello senny!");
-			}
-			
+			} break;
+			default: JOptionPane.showMessageDialog(null, "Hello user " + M); break;}
+			case 1: String A = JOptionPane.showInputDialog("Name");
+				switch (A) {
+					case "Mikoto": JOptionPane.showMessageDialog(null, "Hello " + A + " sadly fictional :("); break;
+					default:JOptionPane.showMessageDialog(null, "Hello " + A + ",fellow non-human");
+			} break;
+			case 2: JOptionPane.showMessageDialog(null, "Program Terminated");
 		}
-
+		
+		
+		
+		
 	}
 }
