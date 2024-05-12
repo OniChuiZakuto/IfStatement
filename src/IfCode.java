8eaa1e5b37de76d2648854feaee322607407ab58
Ben <torresjanbenidect@gmail.com>
2024-05-12 18:05:41 +0800

    Extend
    More code inputted and branched

--- src/IfCode.java
+++ src/IfCode.java
@@ -7,28 +7,35 @@ public class IfCode {
 		switch (T) {
 			case 0: 
 			String M = JOptionPane.showInputDialog("Name");
-		switch (M) {
-			case "Mikoto": int m = 9;
-			if (m==9) {
-				System.out.println("Mikoto");
-				JOptionPane.showMessageDialog(null, "HELLO MIKOTO!");
-			} break;
-			case "Jansen": int j = 1;	
-			if (j==1) {
-				System.out.println("Jansen");
-				JOptionPane.showMessageDialog(null, "Hello senny!");
-			} break;
-			default: JOptionPane.showMessageDialog(null, "Hello user " + M); break;}
-			case 1: String A = JOptionPane.showInputDialog("Name");
-				switch (A) {
+				switch (M) {
+					case "Mikoto": int m = 9;
+						if (m==9) {
+								System.out.println("Mikoto");
+								JOptionPane.showMessageDialog(null, "HELLO MIKOTO!");
+						} break;
+					case "Jansen": int j = 1;	
+						if (j==1) {
+							System.out.println("Jansen");
+							JOptionPane.showMessageDialog(null, "Hello senny!");
+						} break;
+					default: JOptionPane.showMessageDialog(null, "Hello user " + M);} break;
+			case 1:
+				String A = JOptionPane.showInputDialog("Name");
+				switch  (A) {
 					case "Mikoto": JOptionPane.showMessageDialog(null, "Hello " + A + " sadly fictional :("); break;
 					default:JOptionPane.showMessageDialog(null, "Hello " + A + ",fellow non-human");
-			} break;
-			case 2: JOptionPane.showMessageDialog(null, "Program Terminated");
+				} break;
+			case 2: JOptionPane.showMessageDialog(null, "Program Terminated"); break;}
+			int State = T;
+				if (State==0 || State==1) {
+					int Age = Integer.parseInt(JOptionPane.showInputDialog("Age"));
+				if (Age<=18) {
+					JOptionPane.showMessageDialog("You are a Minor, Program will be terminated");
+				}
+				else {
+					
+				}
+				}
+				else{}
 		}
-		
-		
-		
-		
 	}
-}
